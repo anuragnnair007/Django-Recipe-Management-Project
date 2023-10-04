7669d7044b66cd0da3f34d50d1b5a834a6f95c60
Anurag N Nair <78994285+anuragnnair007@users.noreply.github.com>
2023-10-04 11:54:12 +0530

    Create views.py
    Views for the project

---
+++ recipe/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render,redirect
+from .models import *
+from django.http import HttpResponse
+
+# Create your views here.
+
+def recipes(request):
+    if request.method == "POST":
+        data= request.POST
+        recipe_image=request.FILES.get('recipe image')
+        recipe_name=data.get('recipe name')
+        recipe_desc= data.get('recipe desc')
+
+        Recipe.objects.create( 
+            recipe_image=recipe_image,
+            recipe_desc=recipe_desc,
+            recipe_name=recipe_name
+        )
+
+        return redirect('/recipes/')
+    
+    queryset= Recipe.objects.all()
+
+    if request.GET.get('search'):
+        queryset= queryset.filter(recipe_name__icontains= request.GET.get('search'))
+
+    context= {'recipes':queryset}
+    return render(request, 'recipes.html',context)
+
+def update_recipe(request,id):
+    queryset= Recipe.objects.get(id=id)
+
+    if request.method == "POST":
+        data= request.POST
+        recipe_image=request.FILES.get('recipe image')
+        recipe_name=data.get('recipe name')
+        recipe_desc= data.get('recipe desc')
+
+        queryset.recipe_desc=recipe_desc
+        queryset.recipe_name=recipe_name
+
+        if recipe_image:
+            queryset.recipe_image=recipe_image
+
+        queryset.save()
+        return redirect('/recipes/')
+    
+    context= {'recipe':queryset}
+    return render(request, 'update_recipe.html', context)
+
+
+def delete_recipe(request,id):
+    print(id)
+    recipe= Recipe.objects.get(id=id)
+    recipe.delete()
+    return redirect('/recipes/')
+
+
